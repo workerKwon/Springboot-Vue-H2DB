4642e61adab61be4234e812e3501fc7022eab598
권태일 <xodlf0415@jmsight.com>
2020-03-24 10:54:45 +0900

    spring boot, vue, h2 설정 완료

---
+++ src/main/java/com/example/springboot_vue_h2db/SpringbootVueH2dbApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_vue_h2db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootVueH2dbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootVueH2dbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboot_vue_h2db/SpringbootVueH2dbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_vue_h2db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootVueH2dbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
