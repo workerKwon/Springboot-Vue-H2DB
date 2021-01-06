beccf7fae42606953f61515bca2f2bdb7b82f211
workerKwon <dmdmdm1322@gmail.com>
2021-01-06 15:26:34 +0900

    백엔드 포트 변경 property 추가

--- vue/src/http-common.js
+++ vue/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:8080/_api',
+  baseURL: 'http://localhost:8000/_api',
   headers: {
     'Content-type': 'application/json',
   },
